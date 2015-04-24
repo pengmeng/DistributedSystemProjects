a67550e3a3389240eeeb675ba8272ad8089b9b77
pengmeng <mengp3157@gmail.com>
2015-04-24 11:03:37 -0500

    update test function call

--- src/kademlia/kademlia_test.go
+++ src/kademlia/kademlia_test.go
@@ -15,7 +15,7 @@ func Test_kBuckets(t *testing.T) {
 	go AddrBook.HandleContact(conCh)
 	remoteID := NewRandomID()
 	conCh <- &Contact{remoteID, net.IPv4(127, 0, 0, 1), 7809}
-	C, _ := AddrBook.Find(remoteID)
+	C, _ := AddrBook.Find_helper(remoteID)
 	if !C.NodeID.Equals(remoteID) {
 		t.Error("Error!")
 	}
