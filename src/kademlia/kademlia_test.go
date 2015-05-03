fdb494200356b7e6166f36b9105b9c8a60bc1d92
pengmeng <mengp3157@gmail.com>
2015-05-03 00:45:00 -0500

    add demo test

--- src/kademlia/kademlia_test.go
+++ src/kademlia/kademlia_test.go
@@ -1,75 +1,52 @@
 package kademlia
 
 import (
-	"bytes"
-	"fmt"
-	"net"
-	"testing"
+    "testing"
+    "net"
+    "strconv"
 )
 
-func Test_Find(t *testing.T) {
-	id := NewRandomID()
-	AddrBook := BuildKBuckets(Contact{id, net.IPv4(127, 0, 0, 1), 6999})
 
-	// add remote Contact to the address book
-	remoteID := NewRandomID()
-	AddrBook.Update(Contact{remoteID, net.IPv4(127, 0, 0, 1), 7809})
-	C := AddrBook.Find(remoteID)
-	if len(C) == 1 {
-		if !C[0].NodeID.Equals(remoteID) {
-			t.Error("Return Id not match")
-		}
-	} else {
-		t.Errorf("Return array length is %d\n", len(C))
-	}
-
-	notexistID := NewRandomID()
-	notexist := AddrBook.Find(notexistID)
-	if len(notexist) != 0 {
-		t.Errorf("Return array length is %d\n", len(notexist))
-	}
+func StringToIpPort(laddr string) (ip net.IP, port uint16, err error){
+    hostString, portString, err := net.SplitHostPort(laddr)
+    if err != nil {
+        return
+    }
+    ipStr, err := net.LookupHost(hostString)
+    if err != nil {
+        return
+    }
+    for i := 0; i < len(ipStr); i++ {
+        ip = net.ParseIP(ipStr[i])
+        if ip.To4() != nil {
+            break
+        }
+    }
+    portInt, err := strconv.Atoi(portString)
+    port = uint16(portInt)
+    return
 }
 
-func Test_MultiContacts(t *testing.T) {
-	self := NewRandomID()
-	AddrBook := BuildKBuckets(Contact{self, net.IPv4(127, 0, 0, 1), 6999})
-	start := 7000
-	end := start + 100
-	rdict := make(map[int]int)
-	for i := start; i <= end; i++ {
-		id := NewRandomID()
-		dis := self.Xor(id).PrefixLen()
-		if _, ok := rdict[dis]; ok {
-			rdict[dis] += 1
-		} else {
-			rdict[dis] = 1
-		}
-		AddrBook.Update(Contact{id, net.IPv4(127, 0, 0, 1), uint16(i)})
-	}
-	for k, v := range rdict {
-		fmt.Printf("Distance %d has contacts %d\n", k, v)
-	}
-}
-
-func Test_Kademlia(t *testing.T) {
-	k := NewKademlia("127.0.0.1:7809")
-	key := NewRandomID()
-	value := []byte("hello world")
-
-	k.addData(Pair{key, value})
-
-	res, err := k.getData(key)
-	if !bytes.Equal(res, value) || err != nil {
-		t.Error("Retrieve wrong value!")
-	}
-
-	res, err = k.getData(NewRandomID())
-	if res != nil || err == nil {
-		t.Error("Error dealing with non existent key")
-	}
-
-}
-
-func Test_PING(t *testing.T) {
-
+func TestPing(t *testing.T) {
+    instance1 := NewKademlia("localhost:7890")
+    instance2 := NewKademlia("localhost:7891")
+    host2, port2, _ := StringToIpPort("localhost:7891")
+    instance1.DoPing(host2, port2)
+    contact2, err := instance1.FindContact(instance2.NodeID)
+    if err != nil {
+        t.Error("Instance 2's contact not found in Instance 1's contact list")
+        return
+    }
+    contact1, err := instance2.FindContact(instance1.NodeID)
+    if err != nil {
+        t.Error("Instance 1's contact not found in Instance 2's contact list")
+        return
+    }
+    if contact1.NodeID != instance1.NodeID {
+        t.Error("Instance 1 ID incorrectly stored in Instance 2's contact list")
+    }
+    if contact2.NodeID != instance2.NodeID {
+        t.Error("Instance 2 ID incorrectly stored in Instance 1's contact list")
+    }
+    return
 }
