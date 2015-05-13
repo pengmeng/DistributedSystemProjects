b2d38ddb6216e2a63fbed6d54455ebb30622561a
Dung Nguyen <dzungng89@gmail.com>
2015-05-12 20:04:54 -0500

    Test cases for iterative RPCs

--- src/kademlia/kademlia_test.go
+++ src/kademlia/kademlia_test.go
@@ -4,9 +4,11 @@ import (
     "testing"
     "net"
     "strconv"
+    "fmt"
+    "bytes"
 )
 
-
+// ======================= Primitive operations ===========================
 func StringToIpPort(laddr string) (ip net.IP, port uint16, err error){
     hostString, portString, err := net.SplitHostPort(laddr)
     if err != nil {
@@ -50,3 +52,101 @@ func TestPing(t *testing.T) {
     }
     return
 }
+
+func TestStoreFind(t *testing.T) {
+    instance1 := NewKademlia("localhost:7892")
+    instance2 := NewKademlia("localhost:7893")
+
+    id := NewRandomID()
+    value := []byte("hello world!")
+
+    // Primitive DoStore
+    instance1.DoStore(&instance2.SelfContact, id, value)
+    
+    res := instance2.LocalFindValue(id)
+    if res != ("OK: Found value: " + string(value)) {
+        t.Error("Instance 2 store wrong value!")
+    }
+
+    // Primitive DoFindValue
+    res = instance1.DoFindValue(&instance2.SelfContact, id)
+    if res != ("OK: Found value: " + string(value)) {
+        t.Error("DoFindValue from Instance 1 retrieve wrong value!")   
+    }
+
+    // Some problem with gob error??
+    // res = instance1.DoFindValue(&instance2.SelfContact, NewRandomID())
+    // fmt.Println(res)
+    // // if res != ("OK: Found value: " + string(value)) {
+    // //     t.Error("DoFindValue from Instance 1 retrieve wrong value!")   
+    // // }
+}
+
+
+// =============================== Iterative ==============================
+// Global variable, any better design??
+var instance = SetUpNetwork()
+
+func SetUpNetwork() []*Kademlia {
+    // Create the network
+    ports := []uint16{7894, 7895, 7896, 7897, 7898, 7899}
+    var instance []*Kademlia
+
+    for _, p := range ports{
+        instance = append(instance, NewKademlia("localhost:" + strconv.Itoa(int(p))))
+    }
+
+    for i := 1; i < len(ports); i++ {
+        instance[i].DoPing(net.IPv4(127,0,0,1), ports[i - 1])
+    }
+    fmt.Println("Done creating network")
+    return instance
+}
+
+func TestIterativeFindNode(t *testing.T) {
+    // Use Node ID to look up, Need to return that node
+    N := len(instance)
+
+    for i := 0; i < N; i++ {
+        //if j := i it's not working
+        for j := i + 1; j < N; j++ {
+            contacts := instance[i].iterativeFindNode(instance[j].NodeID)
+            found := false
+            for _, c := range contacts {
+                if  c.Equal(instance[j].SelfContact) {
+                    found = true
+                }
+            }
+            if !found {
+                t.Error(fmt.Sprintf("Iterative Find couldn't find node %d from node %d", j, i))
+            }        
+        }
+    }
+}
+
+func TestDoIterativeStoreAndIterativeFind(t *testing.T) {
+    N := len(instance)
+
+    for i := 0; i < N; i++ {
+        //if j := i it's not working
+        for j := i + 1; j < N; j++ {
+     
+            key := instance[j].NodeID
+            value := []byte(NewRandomID().AsString())
+            
+            instance[i].DoIterativeStore(key, value)
+
+            res := instance[j].LocalFindValue(key)
+            if res != ("OK: Found value: " + string(value)) {
+                t.Error(fmt.Sprintf("Node %d doesn't store correctly key,value from node %d", j, i))
+            }
+
+            retrieve := instance[i].DoIterativeFindValue(key)
+            if !bytes.Equal(value, []byte(retrieve)) {
+                t.Error(fmt.Sprintf("Error retrieving data from node %d", i))
+            }
+        }
+    }
+}
+
+
